90fcf8b30ea44904da375ccdd8e4d5d2e117aace
FurtakM <serpentow@gmail.com>
2020-12-07 02:31:38 +0100

    1.4 prebuild-23

--- lua/sos_video.lua
+++ lua/sos_video.lua
@@ -5,20 +5,21 @@ end;
 
 function inGameOnEffect(VIS, CALLBACK)
 	if (VIS == 1) then
-		AddEventFade(game.filmtop.ID, 255, 1, CALLBACK);
-		AddEventFade(game.filmbottom.ID, 255, 1, CALLBACK);
-		AddEventFade(game.ui.ID, 0, 1);
+		AddEventFade(game.filmtop.ID, 255, 0.5, CALLBACK);
+		AddEventFade(game.filmbottom.ID, 255, 0.5, CALLBACK);
+		AddEventFade(game.ui.ID, 0, 0.5);
 		setVisible(game.ui, VIS == 0);
 	else
 		setVisible(game.ui, VIS == 0);
-		AddEventFade(game.filmtop.ID, 0, 1, CALLBACK);
-		AddEventFade(game.filmbottom.ID, 0, 1, CALLBACK);
-		AddEventFade(game.ui.ID, 255, 1);
+		AddEventFade(game.filmtop.ID, 0, 0.5, CALLBACK);
+		AddEventFade(game.filmbottom.ID, 0, 0.5, CALLBACK);
+		AddEventFade(game.ui.ID, 255, 0.5);
 	end;
 end;
 
 setAlpha(game.filmtop, 0);
 setAlpha(game.filmbottom, 0);
+setHeight(game.filmbottom, getHeight(game.filmbottom) + 190);
 
 function FROMOW_SHOWFILMS(VIS) -- OW CALLS THIS
 	if (VIS) then		
@@ -34,5 +35,5 @@ end;
 
 function FROMOW_SETGAMEWINDOW(W, H, T)
     set_Property(gamewindow.ID, PROP_W, W);
-    set_Property(gamewindow.ID, PROP_H, H);
+    set_Property(gamewindow.ID, PROP_H, H - 190);
 end;
