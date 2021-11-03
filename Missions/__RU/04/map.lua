6579c7a6abc8ae1da73d4b70b20272b1b6e67ced
Nefarem <33377304+Nefarem@users.noreply.github.com>
2021-11-03 19:56:42 +0100

    Init Ru files

---
+++ Missions/__RU/04/map.lua
@@ -0,0 +1,7 @@
+function FROMOW_MAP_START(MAPPATH)
+    registryAchievs({'ACH_FIST', 'ACH_COMMISAR'});
+end;
+
+function FROMOW_MAP_END()
+    clearRegistryAchievs();
+end;
---
+++ Missions/__RU/05_cont/map.lua
@@ -0,0 +1,9 @@
+function FROMOW_MAP_START(MAPPATH)
+	registryAchievs({'ACH_SNI'});
+end;
+
+
+
+function FROMOW_MAP_END()
+	clearRegistryAchievs();
+end;
