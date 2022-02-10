a9df226283f811c263d74d09512dcb5fe823761b
FurtakM <serpentow@gmail.com>
2022-02-10 22:58:19 +0100

    hack mod

--- lua/sos_hack.lua
+++ lua/sos_hack.lua
@@ -190,6 +190,169 @@ function spawnVehicle()
     );
 end;
 
+BASE_ACTIVE_NATION = 1;
+BASE_ACTIVE_BUILDING = 1;
+BASE_BUILDING_LIST = {
+    'Depot',
+    'Warehouse',
+    'Lab',
+    'Lab (Weapon)',
+    'Lab (Sib)',
+    'Lab (Comp)',
+    'Lab (Opto)',
+    'Lab (Space)',
+    'Lab (Bio)',
+    'Lab (Weapon + Sib)',
+    'Lab (Comp + Opto)',
+    'Lab (Space + Comp)',
+    'Lab (Bio + Opto)',
+    'Workshop',
+    'Factory',
+    'Ext. Gun',
+    'Ext. Cargo',
+    'Ext. Radar',
+    'Ext. Radio',
+    'Ext. Sib',
+    'Ext. Comp',
+    'Ext. Track',
+    'Ext. Laser',
+    'Control Tower',
+    'Bunker',
+    'Tower',
+    'Automatic Turret',
+    'Armoury',
+    'Barracks',
+    'Solar Plant',
+    'Oil Plant',
+    'Sib Plant',
+    'Oil Mine',
+    'Sib Mine'
+};
+
+BASE_DIRECTION_TEXTURES = {
+    'up-right-',
+    'right-',
+    'down-right-',
+    'down-left-',
+    'left-',
+    'up-left-'
+};
+BASE_ACTIVE_DIRECTION_LAST = -1;
+BASE_ACTIVE_DIRECTION = 0;
+BASE_ACTIVE_WEAPON = 1;
+BASE_WEAPON_LIST = {
+    '(US) Machine Gun', -- 2
+    '(US) Light Gun', -- 3
+    '(US) Gatling Gun', -- 4
+    '(US) Double Gun', -- 5
+    '(US) Heavy Gun', -- 6
+    '(US) Rocket Launcher', -- 7
+    '(US) Sib Rocket', -- 8
+    '(US) Laser', -- 9
+    '(US) Double Laser', -- 10
+    '(US) Radar', -- 11
+    '(US) Artillery', -- 97
+    '(AR) Ballista', -- 22
+    '(AR) Light Gun', -- 23
+    '(AR) Double M. Gun', -- 24
+    '(AR) Gatling Gun', -- 25
+    '(AR) Flame Thrower', -- 26
+    '(AR) Gun', -- 27
+    '(AR) Rocket Launcher', -- 28
+    '(AR) Radar', -- 30
+    '(AR) Mortar', -- 92,
+    '(RU) Heavy M. Gun', -- 42
+    '(RU) Gatling Gun', -- 43
+    '(RU) Gun', -- 44
+    '(RU) Rocket Launcher', -- 45
+    '(RU) Heavy Gun', -- 46
+    '(RU) Rocket', -- 47
+    '(RU) Sib Rocket', -- 48
+    '(RU) Time Lapser', -- 49
+    '(RU) Flame Thrower', -- 93
+    '(RU) Radar', -- 98
+};
+
+function changeBaseNation(ID, INDEX)
+    BASE_ACTIVE_NATION = parseInt(INDEX);
+end;
+
+function changeBaseBuilding(ID, INDEX)
+    BASE_ACTIVE_BUILDING = parseInt(INDEX);
+end;
+
+function changeBaseWeapon(ID, INDEX)
+    local WEAPON = parseInt(INDEX);
+    local LIST = {
+        2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 97, -- US
+        22, 23, 24, 25, 26, 27, 28, 30, 91, -- AR
+        42, 43, 44, 45, 46, 47, 48, 49, 93, 98 -- RU
+    };
+
+    BASE_ACTIVE_WEAPON = LIST[WEAPON];
+end;
+
+function changeBaseDirection(ID, DIR)
+    DIR = parseInt(DIR);
+    setTextureID(BASE_ACTIVE_DIRECTION_LAST, 'SGUI/Hack/arrows/' .. BASE_DIRECTION_TEXTURES[BASE_ACTIVE_DIRECTION + 1] .. '2');
+    
+    BASE_ACTIVE_DIRECTION = DIR;
+    BASE_ACTIVE_DIRECTION_LAST = ID;
+
+    setTextureID(BASE_ACTIVE_DIRECTION_LAST, 'SGUI/Hack/arrows/' .. BASE_DIRECTION_TEXTURES[DIR + 1] .. '1');
+end;
+
+function spawnBuilding()
+    OW_COMM_BUTTON_PRESS(
+        IT_TARGETABLESAIL_NOUNIT, 
+        303, 
+        BASE_ACTIVE_NATION,
+        BASE_ACTIVE_BUILDING,
+        BASE_ACTIVE_DIRECTION,
+        BASE_ACTIVE_WEAPON
+    );
+end;
+
+-- behaviour functions
+function teleportUnit()
+    OW_COMM_BUTTON_PRESS(
+        IT_TARGETABLESAIL, 
+        304
+    );
+end;
+
+function unitInvincible()
+    OW_CUSTOM_COMMAND(101, 4);
+end;
+
+function unitInvisible()
+    OW_CUSTOM_COMMAND(101, 5);
+end;
+
+function unitSide()
+    OW_CUSTOM_COMMAND(101, 7);
+end;
+
+function unitLevelMax()
+    OW_CUSTOM_COMMAND(101, 2);
+end;
+
+function unitYourLevelMax()
+    OW_CUSTOM_COMMAND(101, 3);
+end;
+
+-- map functions
+function mapFog()
+    OW_CUSTOM_COMMAND(101, 8);
+end;
+
+function mapChangeSide()
+    OW_CUSTOM_COMMAND(101, 6);
+end;
+
+function mapResources()
+    OW_CUSTOM_COMMAND(101, 1);
+end;
 
 -- main functions
 function showHumanPanel(MODE)
@@ -208,6 +371,36 @@ function showVehiclePanel(MODE)
         setVisible(game.hackPanel.main, false);
     else
         setVisible(game.hackPanel.vehiclePanel, false);
+        setVisible(game.hackPanel.main, true);
+    end;
+end;
+
+function showBasePanel(MODE)
+    if (MODE == 1) then
+        setVisible(game.hackPanel.basePanel, true);
+        setVisible(game.hackPanel.main, false);
+    else
+        setVisible(game.hackPanel.basePanel, false);
+        setVisible(game.hackPanel.main, true);
+    end;
+end;
+
+function showBehaviourPanel(MODE)
+    if (MODE == 1) then
+        setVisible(game.hackPanel.behavPanel, true);
+        setVisible(game.hackPanel.main, false);
+    else
+        setVisible(game.hackPanel.behavPanel, false);
+        setVisible(game.hackPanel.main, true);
+    end;
+end;
+
+function showMapPanel(MODE)
+    if (MODE == 1) then
+        setVisible(game.hackPanel.mapPanel, true);
+        setVisible(game.hackPanel.main, false);
+    else
+        setVisible(game.hackPanel.mapPanel, false);
         setVisible(game.hackPanel.main, true);
     end;
 end;
@@ -599,5 +792,380 @@ game.hackPanel.vehiclePanel.buttonBack = clButton(
     20,
     'Back',
     'showVehiclePanel(0);',
+    {}
+);
+
+-- base panel
+game.hackPanel.basePanel = getElementEX(
+    game.hackPanel, 
+    anchorNone, 
+    XYWH(0, 30, getWidth(game.hackPanel), getHeight(game.hackPanel) - 30), 
+    false,
+    {
+        colour1 = WHITEA()
+    }
+);
+
+game.hackPanel.basePanel.name = getLabelEX(
+    game.hackPanel.basePanel,
+    anchorNone,
+    XYWH(0, 10, 300, 10), 
+    nil,
+    'Base Panel',
+    {
+        nomouseevent = true,
+        text_halign = ALIGN_MIDDLE,
+        text_valign = ALIGN_TOP,
+    }
+);
+
+game.hackPanel.basePanel.nationLabel = getLabelEX(
+    game.hackPanel.basePanel,
+    anchorT, 
+    XYWH(
+        10,
+        30,
+        240,
+        14
+    ), 
+    nil,
+    'Nation',
+    {
+        nomouseevent = true
+    }
+);
+
+game.hackPanel.basePanel.nationCombo = clComboBox(
+    game.hackPanel.basePanel,
+    10,
+    50,
+    HUMAN_NATION_LIST,
+    1,
+    'changeBaseNation(%id, "INDEX")',
+    {}
+);
+
+game.hackPanel.basePanel.btypeLabel = getLabelEX(
+    game.hackPanel.basePanel,
+    anchorT, 
+    XYWH(
+        10,
+        70,
+        240,
+        14
+    ), 
+    nil,
+    'Building',
+    {
+        nomouseevent = true
+    }
+);
+
+game.hackPanel.basePanel.btypeCombo = clComboBox(
+    game.hackPanel.basePanel,
+    10,
+    90,
+    BASE_BUILDING_LIST,
+    1,
+    'changeBaseBuilding(%id, "INDEX")',
+    {}
+);
+
+game.hackPanel.basePanel.bweaponLabel = getLabelEX(
+    game.hackPanel.basePanel,
+    anchorT, 
+    XYWH(
+        10,
+        110,
+        240,
+        14
+    ), 
+    nil,
+    'Weapon',
+    {
+        nomouseevent = true
+    }
+);
+
+game.hackPanel.basePanel.bweaponCombo = clComboBox(
+    game.hackPanel.basePanel,
+    10,
+    130,
+    BASE_WEAPON_LIST,
+    1,
+    'changeBaseWeapon(%id, "INDEX")',
+    {}
+);
+
+game.hackPanel.basePanel.directionLabel = getLabelEX(
+    game.hackPanel.basePanel,
+    anchorT, 
+    XYWH(
+        10,
+        150,
+        240,
+        14
+    ), 
+    nil,
+    'Direction',
+    {
+        nomouseevent = true
+    }
+);
+
+game.hackPanel.basePanel.direction1 = getElementEX(
+    game.hackPanel.basePanel, 
+    anchorNone, 
+    XYWH(10, 175, 30, 30), 
+    true,
+    {
+        texture = 'SGUI/Hack/arrows/up-right-2.png',
+        callback_mousedown = 'changeBaseDirection(%id, 0);'
+    }
+);
+
+game.hackPanel.basePanel.direction2 = getElementEX(
+    game.hackPanel.basePanel, 
+    anchorNone, 
+    XYWH(50, 175, 30, 30), 
+    true,
+    {
+        texture = 'SGUI/Hack/arrows/right-2.png',
+        callback_mousedown = 'changeBaseDirection(%id, 1);'
+    }
+);
+
+game.hackPanel.basePanel.direction3 = getElementEX(
+    game.hackPanel.basePanel, 
+    anchorNone, 
+    XYWH(90, 175, 30, 30), 
+    true,
+    {
+        texture = 'SGUI/Hack/arrows/down-right-2.png',
+        callback_mousedown = 'changeBaseDirection(%id, 2);'
+    }
+);
+
+game.hackPanel.basePanel.direction4 = getElementEX(
+    game.hackPanel.basePanel, 
+    anchorNone, 
+    XYWH(130, 175, 30, 30), 
+    true,
+    {
+        texture = 'SGUI/Hack/arrows/down-left-2.png',
+        callback_mousedown = 'changeBaseDirection(%id, 3);'
+    }
+);
+
+game.hackPanel.basePanel.direction5 = getElementEX(
+    game.hackPanel.basePanel, 
+    anchorNone, 
+    XYWH(170, 175, 30, 30), 
+    true,
+    {
+        texture = 'SGUI/Hack/arrows/left-2.png',
+        callback_mousedown = 'changeBaseDirection(%id, 4);'
+    }
+);
+
+game.hackPanel.basePanel.direction6 = getElementEX(
+    game.hackPanel.basePanel, 
+    anchorNone, 
+    XYWH(210, 175, 30, 30), 
+    true,
+    {
+        texture = 'SGUI/Hack/arrows/up-left-2.png',
+        callback_mousedown = 'changeBaseDirection(%id, 5);'
+    }
+);
+
+game.hackPanel.basePanel.buttonSpawn = clButton(
+    game.hackPanel.basePanel,
+    10, 
+    220, 
+    280, 
+    20,
+    'Create building',
+    'spawnBuilding();',
+    {}
+);
+
+game.hackPanel.basePanel.buttonBack = clButton(
+    game.hackPanel.basePanel,
+    10, 
+    260, 
+    280, 
+    20,
+    'Back',
+    'showBasePanel(0);',
+    {}
+);
+
+-- base panel
+game.hackPanel.behavPanel = getElementEX(
+    game.hackPanel, 
+    anchorNone, 
+    XYWH(0, 30, getWidth(game.hackPanel), getHeight(game.hackPanel) - 30), 
+    false,
+    {
+        colour1 = WHITEA()
+    }
+);
+
+game.hackPanel.behavPanel.name = getLabelEX(
+    game.hackPanel.behavPanel,
+    anchorNone,
+    XYWH(0, 10, 300, 10), 
+    nil,
+    'Behaviour Panel',
+    {
+        nomouseevent = true,
+        text_halign = ALIGN_MIDDLE,
+        text_valign = ALIGN_TOP,
+    }
+);
+
+game.hackPanel.behavPanel.buttonTeleport = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    40, 
+    280, 
+    20,
+    'Teleport',
+    'teleportUnit();',
+    {}
+);
+
+game.hackPanel.behavPanel.buttonInvincible = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    70, 
+    280, 
+    20,
+    'Invincible',
+    'unitInvincible();',
+    {}
+);
+
+game.hackPanel.behavPanel.buttonInvisible = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    100, 
+    280, 
+    20,
+    'Invisible',
+    'unitInvisible();',
+    {}
+);
+
+game.hackPanel.behavPanel.buttonSide = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    130, 
+    280, 
+    20,
+    'Change Unit Side',
+    'sideUnit();',
+    {}
+);
+
+game.hackPanel.behavPanel.buttonLevel = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    160, 
+    280, 
+    20,
+    'Set level 10 for selected units',
+    'unitLevelMax();',
+    {}
+);
+
+game.hackPanel.behavPanel.buttonYourLevel = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    190, 
+    280, 
+    20,
+    'Set level 10 for your units',
+    'unitYourLevelMax();',
+    {}
+);
+
+game.hackPanel.behavPanel.buttonBack = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    260, 
+    280, 
+    20,
+    'Back',
+    'showBehavPanel(0);',
+    {}
+);
+
+-- map panel
+game.hackPanel.mapPanel = getElementEX(
+    game.hackPanel, 
+    anchorNone, 
+    XYWH(0, 30, getWidth(game.hackPanel), getHeight(game.hackPanel) - 30), 
+    false,
+    {
+        colour1 = WHITEA()
+    }
+);
+
+game.hackPanel.mapPanel.name = getLabelEX(
+    game.hackPanel.mapPanel,
+    anchorNone,
+    XYWH(0, 10, 300, 10), 
+    nil,
+    'Map Panel',
+    {
+        nomouseevent = true,
+        text_halign = ALIGN_MIDDLE,
+        text_valign = ALIGN_TOP,
+    }
+);
+
+game.hackPanel.mapPanel.buttonResources = clButton(
+    game.hackPanel.mapPanel,
+    10, 
+    40, 
+    280, 
+    20,
+    'Resources',
+    'mapResources();',
+    {}
+);
+
+game.hackPanel.mapPanel.buttonChangeSide = clButton(
+    game.hackPanel.mapPanel,
+    10, 
+    70, 
+    280, 
+    20,
+    'Change Your Side',
+    'mapChangeSide();',
+    {}
+);
+
+game.hackPanel.mapPanel.buttonFog = clButton(
+    game.hackPanel.mapPanel,
+    10, 
+    100, 
+    280, 
+    20,
+    'Fog Off',
+    'mapFog();',
+    {}
+);
+
+game.hackPanel.behavPanel.buttonBack = clButton(
+    game.hackPanel.behavPanel,
+    10, 
+    260, 
+    280, 
+    20,
+    'Back',
+    'showBehavPanel(0);',
     {}
 );
