4c1632e684cba8f5d11bd57cce5f8709b6e2537e
FurtakM <serpentow@gmail.com>
2024-10-13 01:50:48 +0200

    13.10.24

---
+++ Missions/_Multiplayer/Last Hope/MultiDescCoop.lua
@@ -0,0 +1,4 @@
+isCoop = true;
+banColour = { [1] = 5, [2] = 2 };
+banRandomColours = true;
+forceToPickColours = true;
---
+++ Missions/_Multiplayer/Wipeout/MultiDescCoop.lua
@@ -0,0 +1,4 @@
+isCoop = true;
+banColour = { [1] = 1, [2] = 2 };
+banRandomColours = true;
+forceToPickColours = true;
